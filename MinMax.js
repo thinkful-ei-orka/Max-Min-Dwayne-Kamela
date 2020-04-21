cd68e129b383106363cf6d52ee1a41e152c49d1a
Kamela Williamson <kami63116@yahoo.com>
2020-04-21 11:57:22 -0700

    changes made

--- MinMax.js
+++ MinMax.js
@@ -11,5 +11,20 @@ function Max(numbers) {
 }
 
 function Min(numbers) {
+    let smallest = numbers[0];
+    let i = 0;
+
+    while (i < numbers.length) {
+        if(numbers[i] < smallest) {
+            smallest = numbers[i];
+        }
+        i++;
+    }
+    return smallest;
 
 }
+
+const figures = [1, 6, 14, 7, 12];
+
+console.log(Max(figures));
+console.log(Min(figures));
